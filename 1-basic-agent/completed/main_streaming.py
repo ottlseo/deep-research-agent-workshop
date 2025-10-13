9302ae1100e301194c0eaf03e4229ab4dc625256
ottlseo <dotsi@ewhain.net>
2025-10-13 14:06:24 +0900

    Feat: update completed code in 1-basic-agent

--- 1-basic-agent/completed/main_streaming.py
+++ 1-basic-agent/completed/main_streaming.py
@@ -6,18 +6,17 @@ from prompts.template import apply_prompt_template
 
 agent_name = "toy_agent"
 
-user_input = "안녕 만나서 반가워. 나는 지금 Strands Agents SDK 공부중이야."
+agent = strands_utils.get_agent(
+    agent_name=agent_name,
+    system_prompts=apply_prompt_template(prompt_name=agent_name, prompt_context={"AGENT_NAME": agent_name}),
+    agent_type="claude-sonnet-3-7", # 사용할 LLM 모델 (고성능 추론) 
+    enable_reasoning=False, # 추론 기능 비활성화 (빠른 응답) 
+    prompt_cache_info=(False, None), # 프롬프트 캐싱 비활성화 (False, None), 활성화 시 (True, "default") 
+    streaming=True, # 실시간 응답 스트리밍 활성화
+)
 
 if __name__ == "__main__":
-    
-    agent = strands_utils.get_agent(
-        agent_name=agent_name,
-        system_prompts=apply_prompt_template(prompt_name=agent_name, prompt_context={"AGENT_NAME": agent_name}),
-        agent_type="claude-sonnet-3-7", # 사용할 LLM 모델 (고성능 추론) 
-        enable_reasoning=False, # 추론 기능 비활성화 (빠른 응답) 
-        prompt_cache_info=(False, None), # 프롬프트 캐싱 비활성화 (False, None), 활성화 시 (True, "default") 
-        streaming=True, # 실시간 응답 스트리밍 활성화
-    )
+    user_input = "안녕 만나서 반가워. 나는 지금 Strands Agents SDK 공부중이야."
 
     async def run_streaming():
         full_text = ""
